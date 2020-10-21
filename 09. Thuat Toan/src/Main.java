d99c4b3e6c1de3556ea22f63bd1678e8a20f2473
TungPx <phamxuantung2789@gmail.com>
2020-10-21 22:51:34 +0700

    a

--- 09. Thuat Toan/src/Main.java
+++ 09. Thuat Toan/src/Main.java
@@ -1,16 +1,29 @@
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
-        int result = digitsProduct(12);
-        System.out.println(result);
+        System.out.println(truncateString("57439552816"));
     }
 
-    private static int digitsProduct(int n) {
-        int number = 0;
-        int[] res;
-        int index = -1;
-        for(int i = 2; i < n; i++){
-            if(n % i == 0)
-        }
-        return number;
+    private static String truncateString(String s) {
+        do{
+            int i = s.length();
+            if (i == 0) break;
+
+            if (toInt(s, 0) % 3 == 0){
+                s = s.substring(1);
+            } else if(toInt(s, i - 1) % 3 == 0){
+                s = s.substring(0, i - 1);
+            } else if ((toInt(s, 0) + toInt(s, i - 1)) % 3 == 0){
+                s = s.substring(1, s.length() - 1);
+            } else
+                break;
+        }while (true);
+        return s;
     }
+
+    private static int toInt(String s, int i) {
+        return Integer.parseInt(String.valueOf(s.charAt(i)));
+    }
+
 }
